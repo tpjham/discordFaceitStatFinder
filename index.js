e2cf0cf9e6c48246c6ad317e574a7626f05e06a4
Tomi H <tomi.pj.hamari@student.jyu.fi>
2021-12-10 18:17:01 +0200

    Update index.js
    Switched the message into an embed for a slightly cleaner look

--- index.js
+++ index.js
@@ -15,10 +15,16 @@ client.once("ready", () => {
 });
 
 client.on("messageCreate", async (message) => {
-  let toSend = ``;
+  let embededInfo = new Discord.MessageEmbed;
+  embededInfo.setDescription("\u2800");
+  embededInfo.setFooter("\u2800".repeat(1000/*any big number works too*/)+"|");
+  let embededInfo2 = new Discord.MessageEmbed;
+  embededInfo2.setDescription("\u2800");
+  embededInfo2.setFooter("\u2800".repeat(1000/*any big number works too*/)+"|");
   if (message.content.startsWith("# userid")) {
     try {
       const test = message.content.split("\n");
+      let lineNumber = 0;
       test.forEach((line) => {
         const rivi = line.split('"');
         if (rivi[2]) {
@@ -47,32 +53,81 @@ client.on("messageCreate", async (message) => {
                       )
                       .catch((err) => console.log("Something went wrong 1"));
                     const playerStats = result2.data;
-                    toSend =
-                      toSend +
-                      `\t\t\t\t\t  Steam nick: ${rivi[1]} (Faceit: ${playerData.nickname})
-                      ELO: ${playerData.games.csgo.faceit_elo}\t\t    (Level: ${playerData.games.csgo.skill_level})
-                      Matches: ${playerStats.lifetime.Matches}\t\t   (Winrate: ${playerStats.lifetime["Win Rate %"]}%)
-                      K/D: ${playerStats.lifetime["Average K/D Ratio"]}\t\t    (HS: ${playerStats.lifetime["Average Headshots %"]}%)\n\n`;
+                    if ( lineNumber < 5 ) {
+                      embededInfo.addFields(
+                        {
+                          "name": `${rivi[1]}`,
+                          value: `ELO: ${playerData.games.csgo.faceit_elo} (Level ${playerData.games.csgo.skill_level})`,
+                          inline: true
+                        },
+                        // {
+                        //   name: "ELO",
+                        //   value: `${playerData.games.csgo.faceit_elo} (Level ${playerData.games.csgo.skill_level})`,
+                        //   inline: true
+                        // },
+                        {
+                          name: "Matches",
+                          value: `${playerStats.lifetime.Matches} (Winrate: ${playerStats.lifetime["Win Rate %"]}%)`,
+                          inline: true
+                        },
+                        {
+                          name: "K/D",
+                          value: `${playerStats.lifetime["Average K/D Ratio"]} (HS: ${playerStats.lifetime["Average Headshots %"]}%)`,
+                          inline: true
+                        }                  
+                      )
+                    } else {
+                      embededInfo2.addFields(
+                        {
+                          "name": `${rivi[1]}`,
+                          value: `ELO: ${playerData.games.csgo.faceit_elo} (Level ${playerData.games.csgo.skill_level})`,
+                          inline: true
+                        },
+                        // {
+                        //   name: "ELO",
+                        //   value: `${playerData.games.csgo.faceit_elo} (Level ${playerData.games.csgo.skill_level})`,
+                        //   inline: true
+                        // },
+                        {
+                          name: "Matches",
+                          value: `${playerStats.lifetime.Matches} (Winrate: ${playerStats.lifetime["Win Rate %"]}%)`,
+                          inline: true
+                        },
+                        {
+                          name: "K/D",
+                          value: `${playerStats.lifetime["Average K/D Ratio"]} (HS: ${playerStats.lifetime["Average Headshots %"]}%)\n`,
+                          inline: true
+                        }                    
+                      )
+                    }
+                    lineNumber++;;
                   } else {
-                    toSend =
-                      toSend +
-                      `\t\t\t\t\tCouldn't find user ${rivi[1]} on faceit!\n\n`;
+                    lineNumber++;;
+                    if ( lineNumber < 5 ) {
+                      embededInfo.addField(`${rivi[1]}`, "Could not be found on faceit", false);
+                    } else {
+                      embededInfo2.addField(`${rivi[1]}`, "Could not be found on faceit", false);
+                    }
                   }
                 })
                 .catch((err) => {
+                  lineNumber++;;
                   console.log(err.status);
-                  toSend =
-                    toSend +
-                    `\t\t\t\t\tCouldn't find user ${rivi[1]} on faceit!\n\n`;
+                  if ( lineNumber < 5 ) {
+                    embededInfo.addField(`${rivi[1]}`, "Could not be found on faceit", false);
+                  } else {
+                    embededInfo2.addField(`${rivi[1]}`, "Could not be found on faceit", false);
+                  }
                 });
             } catch (err) {
+              lineNumber++;;
               console.log("Something went wrong");
             }
           }
         }
       });
       setTimeout(() => {
-        message.channel.send(`\`\`\`\n` + toSend + `\n\`\`\``);
+        message.channel.send({embeds: [embededInfo,embededInfo2]});
       }, 3000);
     } catch (err) {
       console.log(err);
